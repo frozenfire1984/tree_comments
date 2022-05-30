6ea3e3c1abfbb6f48d86c4aad2001af76c2be259
ykononov1984 <ykononov@hill30.com>
2022-05-30 17:18:05 +0300

    js: style of status messages moved to css, comments sorted by time, added author name input, fixed bug with depth, time of comment is completed; html: styles moved to file; global/all: name grammar refactoring (deep => depth), comment info and reply ui improved, other minor edits

--- script.js
+++ script.js
@@ -11,79 +11,95 @@ let comments = [
 		id: 2,
 		owner: 1,
 		body: "Первый коммент",
-		time: 100,
+		time: 1653700000000,
 		author: "Вася",
 	},
 	{
 		id: 5,
 		owner: 2,
 		body: "Ответ на первый коммент",
-		time: 110,
+		time: 1653702002000,
 		author: "Петя",
 	},
 	{
 		id: 10,
 		owner: 2,
 		body: "Тоже Ответ на первый коммент",
-		time: 115,
+		time: 1653702010000,
 		author: "Димон",
 	},
 	{
 		id: 11,
 		owner: 2,
 		body: "Тоже Ответ на первый коммент",
-		time: 120,
+		time: 1653702020000,
 		author: "Колян",
 	},
 	{
 		id: 6,
 		owner: 5,
 		body: "Ответ на Ответ на первый коммент",
-		time: 111,
+		time: 1653703000000,
 		author: "Вася",
 	},
 	{
 		id: 20,
 		owner: 6,
 		body: "Ответ на Ответ на Ответ на Ответ на первый коммент",
-		time: 200,
+		time: 1653704000000,
+		author: "Люся",
+	},
+	{
+		id: 21,
+		owner: 6,
+		body: "Тоже Ответ на Ответ на Ответ на Ответ на первый коммент (Время не соответствует порядку комента в базе)",
+		time: 1653703000000,
 		author: "Люся",
 	},
 	{
 		id: 40,
 		owner: 20,
 		body: "Ответ на Ответ на Ответ на Ответ на Ответ на первый коммент",
-		time: 220,
+		time: 1653705000000,
 		author: "John",
 	},
 	{
 		id: 3,
 		owner: 1,
 		body: "Второй коммент",
-		time: 101,
+		time: 1653701000000,
 		author: "Гоги",
 	},
 	{
 		id: 4,
 		owner: 1,
 		body: "Третий коммент",
-		time: 102,
+		time: 1653702000000,
 		author: "Вова",
 	},
 	{
 		id: 30,
 		owner: 3,
 		body: "Ответ на второй коммент",
-		time: 300,
+		time: 1653702001000,
 		author: "Леня",
 	},
+	{
+		id: 50,
+		owner: 1,
+		body: "Ответ из другого часового пояса (Время не соответствует порядку комента в базе)",
+		time: 1651000000000,
+		author: "Вася",
+	},
 ]
+
+comments.sort((a, b) => a.time - b.time);
 
 const comments_tag = document.querySelector("#comments")
 const form_main_tag = document.querySelector("#form-main")
 const form_main_submit_tag = document.querySelector("#form-main_submit")
 let queue = [comments[0].id]
-const max_deep = 4
+const max_depth = 6
 
 let temp_id = 1000
 
@@ -96,8 +112,10 @@ const send_comment_func = (e, mode) => {
 	e.preventDefault()
 	//console.log(e)
 	const form_tag = e.currentTarget
-	const status_tag = form_tag.querySelector(".comment__status")
-	const input_tag = form_tag.querySelector(".comment__input")
+	const details_tag = form_tag.closest(".comment").querySelector(".comment__details")
+	const status_tag = form_tag.closest(".comment").querySelector(".comment__status")
+	const input_tags = form_tag.querySelectorAll(".comment__input")
+	
 	status_tag.style.removeProperty("opacity")
 	status_tag.textContent = "...sending..."
 	
@@ -114,11 +132,11 @@ const send_comment_func = (e, mode) => {
 	//console.log(payload_for_output)
 	
 	const data_for_output = {
-		author: "Test user",
+		author: payload_for_output.comment_author ? payload_for_output.comment_author : "anonimus",
 		body: payload_for_output.comment_value,
 		id: temp_id++,
 		owner: mode.mode === "primary" ? 1 : payload_for_output.comment_owner,
-		time: new Date(),
+		time: new Date().getTime(),
 	}
 	
 	const send_data = new Promise((resolve, reject) => {
@@ -130,58 +148,65 @@ const send_comment_func = (e, mode) => {
 	
 	send_data
 		.then((data) => {
-			console.log(`sending data: ${data}`)
-			status_tag.style.color = "green"
+			//console.log(`sending data: ${data}`)
+			
+			status_tag.classList.add("comment__status_sus")
 			status_tag.textContent = "commend send successfully"
 			
 			if (mode.mode === "primary") {
-				build_html_func(data_for_output, 1, true, 1, mode)
+				build_html_func(data_for_output, 1, 1, mode)
 			}
 			
 			if (mode.mode === "tree") {
-				build_html_func(data_for_output, parseInt(payload_for_output.comment_owner_deep) + 1, parseInt(payload_for_output.comment_owner_deep) < max_deep, payload_for_output.comment_owner, mode)
+				//console.log(payload_for_output.comment_owner_depth)
+				build_html_func(data_for_output, parseInt(payload_for_output.comment_owner_depth) + 1, payload_for_output.comment_owner, mode)
 			}
 			
-			input_tag.value = null
+			input_tags.forEach((tag => {
+				tag.value = ""
+			}))
 			
 			setTimeout(() => {
 				status_tag.style.opacity = 0
 			}, 2000)
 			setTimeout(() => {
-				status_tag.style.color = null
+				details_tag.open = false
+				status_tag.classList.remove("comment__status_sus")
 				status_tag.textContent = ""
 			}, 3000)
 		})
 		.catch((err) => {
 			console.error(err)
-			status_tag.style.color = "#f00"
+			status_tag.classList.add("comment__status_err")
 			status_tag.textContent = err
 			setTimeout(() => {
 				status_tag.style.opacity = 0
 			}, 2000)
 			setTimeout(() => {
-				status_tag.style.color = null
+				status_tag.classList.remove("comment__status_err")
 				status_tag.textContent = ""
 			}, 3000)
 		})
-	
 }
 
 // TODO: build_html_func
-const build_html_func = (obj, deep, send_permission, owner_id = comments[0].id, mode) => {
+const build_html_func = (obj, depth, owner_id = comments[0].id, mode) => {
 	const template_tag_content = document.querySelector("#template_comments").content.cloneNode(true)
+	
+	console.log(depth)
 	
 	const tags_list = [
 		".comment",
 		".comment__id",
 		".comment__author",
 		".comment__time",
+		".comment__details",
 		".comment__form",
 		".comment__input",
 		".comment__submit",
 		".comment__body",
 		"[name='comment_owner']",
-		"[name='comment_owner_deep']",
+		"[name='comment_owner_depth']",
 	]
 	
 	const [
@@ -189,27 +214,32 @@ const build_html_func = (obj, deep, send_permission, owner_id = comments[0].id, 
 		template_tag_content_id,
 		template_tag_content_author,
 		template_tag_content_time,
+		template_tag_content_details,
 		template_tag_content_form,
 		template_tag_content_input,
 		template_tag_content_submit,
 		template_tag_content_body,
 		template_tag_content_h_owner,
-		template_tag_content_h_owner_deep] = tags_list.map(tag => {
+		template_tag_content_h_owner_depth] = tags_list.map(tag => {
 		return template_tag_content.querySelector(tag)
 	})
 	
-	template_tag_content_id.textContent = obj.id
+	template_tag_content_id.textContent = `${obj.id} --- depth: ${depth}` //service for testing
+	//template_tag_content_id.textContent = obj.id
 	template_tag_content_author.textContent = obj.author
-	template_tag_content_time.textContent = obj.time
+	template_tag_content_time.setAttribute("data-timestamp", obj.time)
+	template_tag_content_time.textContent = new Date(obj.time).toLocaleString()
 	template_tag_content_body.textContent = obj.body
 	template_tag_content_author.textContent = obj.author
 	template_tag_content_h_owner.value = obj.id
-	template_tag_content_h_owner_deep.value = deep
+	template_tag_content_h_owner_depth.value = depth
 	template_tag_content_root.id = `comment_${obj.id}`
-	template_tag_content_root.classList.add(`comment_deep_${deep}`)
+	template_tag_content_root.classList.add(`comment_depth_${depth}`)
 	template_tag_content_form.id = `form_id_${obj.id}`
 	
-	if (send_permission) {
+	if (depth >= max_depth) {
+		template_tag_content_details.remove()
+	} else {
 		template_tag_content_form.addEventListener("submit",
 			(e) => {
 				send_comment_func(e, {mode: "tree"})
@@ -217,10 +247,6 @@ const build_html_func = (obj, deep, send_permission, owner_id = comments[0].id, 
 		)
 	}
 	
-	if (!send_permission) {
-		template_tag_content_input.disabled = true
-		template_tag_content_submit.disabled = true
-	}
 	template_tag_content_root.setAttribute("data-owner", owner_id)
 	
 	if (mode.mode === "page_loading") {
@@ -231,14 +257,10 @@ const build_html_func = (obj, deep, send_permission, owner_id = comments[0].id, 
 	if (mode.mode === "primary") {
 		//console.info("primary")
 		comments_tag.appendChild(template_tag_content)
-		
-		const color = "#04a400"
-		template_tag_content_root.style.borderColor = color
-		template_tag_content_root.style.boxShadow = `inset ${color} 0 0 3px 3px`
+		template_tag_content_root.classList.add("comment_new")
 		
 		setTimeout(() => {
-			template_tag_content_root.style.borderColor = ''
-			template_tag_content_root.style.boxShadow = ''
+			template_tag_content_root.classList.remove("comment_new")
 		}, 5000)
 	}
 	
@@ -252,13 +274,10 @@ const build_html_func = (obj, deep, send_permission, owner_id = comments[0].id, 
 			owner_tag.after(template_tag_content)
 		}
 		
-		const color = "#04a400"
-		template_tag_content_root.style.borderColor = color
-		template_tag_content_root.style.boxShadow = `inset ${color} 0 0 3px 3px`
+		template_tag_content_root.classList.add("comment_new")
 		
 		setTimeout(() => {
-			template_tag_content_root.style.borderColor = ''
-			template_tag_content_root.style.boxShadow = ''
+			template_tag_content_root.classList.remove("comment_new")
 		}, 5000)
 	}
 }
@@ -269,11 +288,13 @@ const make_tree_func = (obj) => {
 		let last_in_queue = queue[queue.length - 1]
 		let nested_comments = obj.filter(item => item.owner === last_in_queue)
 		
+		//console.log(queue.length)
+		//console.log(queue.length < max_depth)
+		
 		if (nested_comments.length) {
 			let current = nested_comments[0]
-			const send_permission = queue.length - 1 < max_deep
-			//console.log(current)
-			build_html_func(current, queue.length, send_permission, last_in_queue, {mode: "page_loading"})
+			
+			build_html_func(current, queue.length, last_in_queue, {mode: "page_loading"})
 			queue.push(current.id)
 			//comments.splice(comments.findIndex(item => item.id === current.id), 1)
 			obj = obj.filter(item => item.id !== current.id)
@@ -304,7 +325,7 @@ get_data
 		make_tree_func(data)
 	})
 	.catch((err) => {
-		comments_tag.style.color = "red"
+		comments_tag.classList.add("comment__status_err")
 		comments_tag.textContent = err
 		console.error(err)
 	})
